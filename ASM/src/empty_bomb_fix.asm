5bca08fb918076fafa4f293a72878236966fefb6
fig02 <fig02srl@gmail.com>
2018-11-11 22:27:56 -0500

    emptybomb refactoring

--- ASM/src/empty_bomb_fix.asm
+++ ASM/src/empty_bomb_fix.asm
@@ -1,17 +1,28 @@
-;hook into what i think is the bomb deconstructor and set the model in hand value correctly
+;when a bomb instance is unloading, write the correct value to "Held Item" to denote the bomb is no longer in hand
 empty_bomb_fix:
 	;displaced
 	lw	a0, 0x001C($sp)
 	
-	addiu    sp, sp, -0x10    ;move sp back 16 bytes
-	sw       t0, 0x04(sp)     ;save values at t0 and t1 to retrieve later
-   	sw       t1, 0x08(sp)
-   	li		 t0, 0x0100FE00   ;load word for value after bomb is gone
-   	lui		 t1, 0x801E		  ;one byte more cause the following offset is signed
-   	sw		 t0, 0xAB70(t1)   ;save word to 801DAB70
-   	li       t0, 0x00 		  ;load byte for value after bomb is gone
-   	sb	 	 t0, 0xAB74(t1)   ;store byte to 801DAB74
-   	lw		 t1, 0x08(sp)	  ;load old values back to t0 and t1
-   	lw		 t0, 0x04(sp)
-   	jr		 ra 			  ;jump back
-   	addiu    sp, sp, 0x10     ;increase stack pointer back to where it was
+   addiu    sp, sp, -0x10
+   sw       t0, 0x04(sp)
+   sw       t1, 0x08(sp)
+   li       t0, 0x0100FE00
+   lui      t1, 0x801E
+   sw       t0, 0xAB70(t1)
+   li       t0, 0x00
+   sb       t0, 0xAB74(t1)
+   lw       t1, 0x08(sp)
+   lw       t0, 0x04(sp)
+   jr       ra
+   addiu    sp, sp, 0x10
+
+;move sp back 16 bytes
+;save values at t0 and t1 to retrieve later
+;load word for value after bomb is gone
+;one byte more cause the following offset is signed
+;save word to 801DAB70
+;load byte for value after bomb is gone
+;store byte to 801DAB74
+;load old values back to t0 and t1
+;jump back
+;increase stack pointer back to where it was
